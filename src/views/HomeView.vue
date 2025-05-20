f1ef3603ae602fcfee8585c77621283cd1ecd1d7
Admin <marioalexvega@gmail.com>
2025-05-19 23:36:22 -0600

    Styles and Create Table

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,18 @@
 <template>
+ <div class="row" >
+   <div class="col-lg-8 offset-lg-2" >
+     <div class="table-responsive">
+       <table class="table table-bordered table-hover">
+         <thead><tr><th>#</th></tr><tr><th>NOMBRE</th></tr><tr><th>DESCRIPCIÓN</th></tr><tr><th>PRECIO</th></tr><tr><th>ACCIONES</th></tr></thead>
+         <tbody class="table-group-divider" id="contenido">
 
+         </tbody>
+       </table>
+     </div>
+   </div>
+ </div>
 </template>
 
 
+<script setup lang="ts">
+</script>
